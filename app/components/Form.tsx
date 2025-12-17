6be1b3dc596bf686eb8ba97ca272a21f5d986c48
Jairo <jairoccostajr@gmail.com>
2025-12-17 20:06:52 -0300

    feat: Adiciona interatividade ao apagar CEP inválido

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -36,82 +36,77 @@ export default function Form() {
     }
   }
 
-  function cepInput(){
-    if(erro == false){
-      return(
-        <input 
-          type="text"
-          required
-          value={cep}
-          placeholder="CEP"
-          className="border-2 border-gray-200 mb-2"
-          onChange={(e) => setCep(e.target.value)}/>        
-      )
-    }else{
-      return(
-        <>
-        <input 
-          type="text"
-          required
-          value={cep}
-          placeholder="CEP"
-          className="border-2 border-red-400 mb-2 bg-red-100"
-          onChange={(e) => setCep(e.target.value)}/>        
-          <p className="text-red-400 mb-2">O CEP informado é inválido.</p>
-        </>
-      )
-    }
-  }
-
   useEffect(() => {
-    if(cep.length == 8){
+    if(cep.length === 8){
       updateInfo();
-      cepInput();
+      return;
     }
+    setErro(false);
   }, [cep])
   
   return (
     <div className="text-black">
-      <form className="flex flex-col bg-white size-200 items-center justify-center">
+      <form className="flex flex-col bg-white size-150 items-center justify-center rounded-2xl">
           <h1 className="text-black text-7xl mb-5">Adress</h1>
           
-          {cepInput()}
+          <input
+          type="text"
+          required
+          maxLength={8}
+          value={cep}
+          placeholder="CEP"
+          className={`border-2 mb-2 outline-hidden py-2 px-3 w-130 ${
+            erro
+              ? "border-red-400 bg-red-100"
+              : "border-gray-200"
+          }`}
+          onChange={
+            (e) => {setCep(e.target.value)}
+          }
+          />
+
+        {erro && (
+          <p className="text-red-400 mb-2">
+            O CEP informado é inválido.
+          </p>
+        )}
 
           <input 
           type="text"
           required
           value={logradouro}
           placeholder="Rua"
-          className="border-2 border-gray-200 mb-2"
+          className="border-2 border-gray-200 mb-2 outline-hidden py-2 px-3 w-130"
           onChange={(e) => setLogradouro(e.target.value)}/>
           <input 
           type="text"
           required
           value={numero}
           placeholder="Número"
-          className="border-2 border-gray-200 mb-2"
+          className="border-2 border-gray-200 mb-2 outline-hidden py-2 px-3 w-130"
           onChange={(e) => setNumero(e.target.value)}/>
           <input 
           type="text"
           required
           value={bairro}
           placeholder="Bairro"
-          className="border-2 border-gray-200 mb-2"
+          className="border-2 border-gray-200 mb-2 outline-hidden py-2 px-3 w-130"
           onChange={(e) => setBairro(e.target.value)}/>
           <input 
           type="text"
           required
           value={uf}
           placeholder="Estado"
-          className="border-2 border-gray-200 mb-2"
+          className="border-2 border-gray-200 mb-2 outline-hidden py-2 px-3 w-130"
           onChange={(e) => setUf(e.target.value)}/>
           <input 
           type="text"
           required
           value={cidade}
           placeholder="Cidade"
-          className="border-2 border-gray-200 mb-2"
+          className="border-2 border-gray-200 mb-2 outline-hidden py-2 px-3 w-130"
           onChange={(e) => setCidade(e.target.value)}/>
+          <h1 className="text-black mt-10">Aluno: Jairo Costa da Cunha Júnior</h1>
       </form>
     </div>
   );
